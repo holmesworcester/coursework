30d3ba6e767598bedd8e524e937c80e38f1dc2fa
Holmes Wilson <hwilson@gmail.com>
2015-09-24 12:09:26 -0400

    added todo

--- 211.rkt
+++ 211.rkt
@@ -1,6 +1,12 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname |211|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+; todo
+; make a way for fire to spread more naturally
+; make a way to win by putting out all fires
+; make a way to run out of water, and to see how much water you have
+
+
 (require 2htdp/image)
 (require 2htdp/universe)
 
