f883e51ab097c1c0b898fed89bd671f8599a484e
Holmes Wilson <hwilson@gmail.com>
2015-11-20 11:15:58 -0500

    coursework

--- 314.rkt
+++ 314.rkt
@@ -1,3 +1,21 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |314|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+; X -> Boolean
+; predicate that tells me if something is an atom
+
+(define (atom? x)
+  (or (number? x) (symbol? x) (string? x)))
+
+; S-expr Symbol Atom -> S-expr
+; replaces all occurrences of old in sexp with new
+ 
+(check-expect (substitute 'world 'hello 0) 'world)
+(check-expect (substitute '(world hello) 'hello 'bye) '(world bye))
+(check-expect (substitute '(((world) bye) bye) 'bye '42) '(((world) 42) 42))
+ 
+(define (substitute sexp old new)
+  (cond
+    [(atom? sexp) (if (eq? sexp old) new sexp)]
+    [else (map (lambda (s) (substitute s old new)) sexp)]))
+
