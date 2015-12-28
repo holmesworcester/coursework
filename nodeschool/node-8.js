34a3298f1e451c2032817908af5506f7d074a3f5
Holmes Wilson <hwilson@gmail.com>
2015-12-28 18:38:43 -0500

    not sure why this isn't working

--- nodeschool/node-8.js
+++ nodeschool/node-8.js
@@ -4,27 +4,17 @@ const url1 = process.argv[2]
 const url2 = process.argv[3]
 const url3 = process.argv[4]
 
-// const urls = [url1, url2, url3]
+const urls = [url1, url2, url3]
+const i = -1 // url index
 
-// for counting completed callbacks
-var urlsCompleted = 0
+// Consumes a response I can pass it as a callback to httpGet.
 
-// run everything.
-http.get(url1, getAndCount)
-http.get(url2, getAndCount)
-http.get(url3, getAndCount)
+doAllRemainingURLs('starting this off!')
 
-// consumes (data, err) and returns data from a request for one URL and contains something that
-// increments the number of callbacks each time, only once all data is collected. 
-function getAndCount (response) {
-     response.pipe(bl(function (err, data) {
-           urlsCompleted = urlsCompleted + 1;
-           console.log('one done!') // increments
-           			return data.toString; // now I think something else is going wrong and the callback functions aren't even happening.
- 		}
-	))
-} 
+function doAllRemainingURLs (response) {
+	console.log(response)
+//	response.on("data", console.log).setEncoding('utf8')
+	i = i + 1;
+	if (i < 3) http.get(urls[i], doAllRemainingURLs)
+}
 
-while (urlsCompleted < 3) {
-	2 * 5; // do whatever
-} console.log('Acumulator works'); // this might need something to split up strings 
