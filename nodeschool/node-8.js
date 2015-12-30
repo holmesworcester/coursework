afcbe540658009c83b1ba11ac4cb0671774c667d
Holmes Wilson <hwilson@gmail.com>
2015-12-30 11:00:09 -0500

    frustrating

--- nodeschool/node-8.js
+++ nodeschool/node-8.js
@@ -7,11 +7,16 @@ const url3 = process.argv[4]
 
 const urls = [url1, url2, url3]
 
+
+function doOneURL(url, callbackforOne) {
+	http.get(url, callbackforOne)	
+}
+
 // takes in a URL and calls the function callbackforOne on the response
 // given by httpget, which is data.
 // now I just don't know how to get the output into the right format.
 
-function doOneURL(url, callbackforOne) {
+function doOneURLasOneLineString(url, callbackforOne) {
 	http.get(url, function callback(response) {
 	response.on("data", callbackforOne).setEncoding('utf8')})
 }
@@ -29,10 +34,10 @@ function doListofURLs(urlList, next) {
 		// remember I am "returning" things with a callback
 		doOneURL(_.first(urlList), function (responseFirst) {
 			doListofURLs(_.rest(urlList), function (responseRest) {
-				next([responseFirst].concat(responseRest))
+				next(responseFirst.data + "\n" + responseRest.data) // can't figure out how to turn these into strings.
 			})
 		})
 	}
 
-doListofURLs(urls, function (rest) {
-	console.log(rest)})
+doListofURLs(urls, function (everything) {
+	console.log(everything)})
